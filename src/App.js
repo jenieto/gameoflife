668f32f9b80c85830cff52f531efec32800e2e70
Jesus Nieto <jesusnieto55@gmail.com>
2018-09-28 19:23:38 +0200

    Initial commit: folder structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
